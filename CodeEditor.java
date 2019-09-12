16626d1d9b8126b9f63d11c75ebe5e6181f105a9
katherinekin <katherineskin@gmail.com>
2019-09-12 13:57:42 -0500

    Fixed cancel button on popup, modularized

--- CodeEditor.java
+++ CodeEditor.java
@@ -105,24 +105,7 @@ class CodeEditor extends JFrame implements ActionListener {
         title.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
             public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-                // TODO: Modularize, this is the same code as "Exit" option
-                textBeforeExit = text.getText();
-                if (textUponOpen.equals(textBeforeExit)) {//no changes have been made, don't need to prompt extra save
-                    title.setVisible(false); //exit the text
-                }
-                else{
-                    //Changes have been made, prompt user to save
-                    int optionChosen = JOptionPane.showConfirmDialog(title, "You've changed your document. Would you like to save before exiting?");
-                    if (optionChosen == JOptionPane.YES_OPTION) {
-                        //reroute to save
-                        saveFile();
-
-                    } else if (optionChosen == JOptionPane.NO_OPTION) {
-                        title.setVisible(false);//Just exit the page. User doesn't want to save.
-                    }
-                    //An else if for Cancel isn't necessary. Hitting cancel already just removes the popup and returns you to your current window.
-                }
-                System.exit(0);
+                exitOptions();
             }
         });
     } 
@@ -141,8 +124,8 @@ class CodeEditor extends JFrame implements ActionListener {
 
                 writeb.flush();
                 writeb.close(); //closes the file
-
-
+                textUponOpen = "";
+                textBeforeExit = "";
             }
             catch(Exception evt)
             {
@@ -164,9 +147,9 @@ class CodeEditor extends JFrame implements ActionListener {
         //But if you want to make changes to that already-saved file, you can't. You'll have to save it as a diff version.
 
         JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());//directs user to home directory
-        jfc.setDialogTitle("Select a java file");//dialog for selecting file will say choose file
+        jfc.setDialogTitle("Save as java file");//dialog for selecting file will say choose file
         jfc.setAcceptAllFileFilterUsed(false);//Boolean to show that it wont accept any file
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Java(.java)","java");//accept only .txt files
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Java(.java)","java");//accept only .java files
         jfc.addChoosableFileFilter(filter);
         int returnValue = jfc.showSaveDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION)
@@ -203,7 +186,25 @@ class CodeEditor extends JFrame implements ActionListener {
         //}
     }
 
+    public void exitOptions() {
+        textBeforeExit = text.getText();
+        if (textUponOpen.equals(textBeforeExit)) {//no changes have been made, don't need to prompt extra save
+            title.setVisible(false); //exit the text
+            System.exit(0);
+        }
+        else{
+            //Changes have been made, prompt user to save
+            int optionChosen = JOptionPane.showConfirmDialog(title, "You've changed your document. Would you like to save before exiting?");
+            if (optionChosen == JOptionPane.YES_OPTION) {
+                //reroute to save
+                saveFile();
 
+            } else if (optionChosen == JOptionPane.NO_OPTION) {
+                title.setVisible(false);//Just exit the page. User doesn't want to save.
+                System.exit(0);
+            }
+        }
+    }
 
     // If a button is pressed 
     public void actionPerformed(ActionEvent e) 
@@ -227,25 +228,9 @@ class CodeEditor extends JFrame implements ActionListener {
         } 
         else if (in.equals("Exit")) 
         {
-            textBeforeExit = text.getText();
-            if (textUponOpen.equals(textBeforeExit)) {//no changes have been made, don't need to prompt extra save
-                title.setVisible(false); //exit the text
-            }
-            else{
-                //Changes have been made, prompt user to save
-                int optionChosen = JOptionPane.showConfirmDialog(title, "You've changed your document. Would you like to save before exiting?");
-                if (optionChosen == JOptionPane.YES_OPTION) {
-                    //reroute to save
-                    saveAsFile();
-
-                } else if (optionChosen == JOptionPane.NO_OPTION) {
-                    title.setVisible(false);//Just exit the page. User doesn't want to save.
-                }
-                //An else if for Cancel isn't necessary. Hitting cancel already just removes the popup and returns you to your current window.
-            }
-            System.exit(0);
-
-        } else if (in.equals("Open")) {
+            exitOptions();
+        }
+        else if (in.equals("Open")) {
             JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());//directs user to home directory
             jfc.setDialogTitle("Select a java file");//dialog for selecting file will say choose file
             jfc.setAcceptAllFileFilterUsed(false);//Boolean to show that it wont accept any file
