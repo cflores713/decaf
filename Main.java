a49ee2b34e4602973d36a906913201b3d7c6585a
pbyrnes01 <44732273+pbyrnes01@users.noreply.github.com>
2019-09-26 15:24:07 -0500

    now running fx and fixed save

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
+//package sample;
 
 import javafx.application.Application;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button; 
-import javafx.scene.layout.*; 
-import javafx.event.ActionEvent; 
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
 import javafx.stage.FileChooser;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.event.EventHandler; 
-import javafx.scene.control.*; 
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import java.io.BufferedReader;
 import java.io.*;
@@ -20,24 +21,27 @@ import javafx.scene.control.TextArea;
 import java.io.FileReader;
 import java.time.LocalDate;
 import java.util.Optional;
-
 public class Main extends Application implements  EventHandler<ActionEvent>{
-	String textUponOpen = "";
+    String textUponOpen = "";
     String textBeforeExit = "";
     String path = "";
 
     public static int windowNum = 1;
-  
-	TextArea text = new TextArea();
 
-	public void start(Stage s) {
+    TextArea text = new TextArea();
+    public void start(Stage s) {
 
-//        BorderPane root = new BorderPane();
 
-//        text.getStyleClass().add("my-text-area");
-        // Add the CSS style class
-		    //creates the frame gui object
+        //Creates a text box object
+        //JTextArea text;
+
+        //creates the frame gui object
         s.setTitle("Decaf Code Editor");
+
+
+
+
+
 
         //-----------------First menu tab file-------------------//
         Menu file = new Menu("File");
@@ -54,7 +58,7 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         file.getItems().add(saveAs);
         file.getItems().add(Remove);
         file.getItems().add(exit);
-		    
+
         newB.setOnAction(this); //action listoners
         open.setOnAction(this);
         save.setOnAction(this);
@@ -62,7 +66,6 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         Remove.setOnAction(this);
         exit.setOnAction(this);
         //-----------------------------------------------------------//
-
         //-----------------First menu tab file-------------------//
         Menu Edit = new Menu("Edit");
         MenuItem Cut = new MenuItem("Cut");
@@ -76,21 +79,21 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         Cut.setOnAction(this); //action listoners
         Copy.setOnAction(this);
         Paste.setOnAction(this);
-        //-----------------------------------------------------------//
 
+        //-----------------------------------------------------------//
         //----------------- Compile tab-------------------//
         Menu Compile = new Menu("Compile");
         MenuItem Build = new MenuItem("Build");
         Compile.getItems().add(Build);
         Compile.setOnAction(this);
         //-----------------------------------------------------------//
-
         //----------------- Execute tab-------------------//
         Menu Execute = new Menu("Execute");
         // Execute.getItems().add(Execute);
         //-----------------------------------------------------------//
-		    
+
         MenuBar mb = new MenuBar();
+
         // add menu to menubar
         mb.getMenus().add(file);
         mb.getMenus().add(Edit);
@@ -98,27 +101,24 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         mb.getMenus().add(Execute);
         // create a VBox
         VBox vb = new VBox(mb);
-        vb.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//        Below is an example for adding orangered text color style to all text within the textArea
-//        We need to add an event listener that listens for the space key to check if user is entering "true" or "false"
-//        But cannot have different colored text in the same area...may need to use an HTMLEditor instead of a textarea
-//        text.getStyleClass().add("orangered");
-        text.setStyle("-fx-text-fill: orangered;");
+
+        text.setPrefHeight(1000);
+        text.setPrefWidth(1000);
         vb.getChildren().add(text);
-
-
-        Scene sc = new Scene(vb, 500, 500);
-
-
+        Scene sc = new Scene(vb, 500,500);
         // set the scene
         s.setScene(sc);
+
         s.show();
-	}
-	
-	        
-	    
-	void saveFile()
-	{
+
+
+
+    }
+
+
+
+    void saveFile()
+    {
         if (!path.equals(""))
         {
             try//checks if the file is able to open and not invalid
@@ -135,12 +135,12 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
             }
             catch(Exception evt)
             {
-            	Alert alert = new Alert(AlertType.ERROR);
-        		alert.setTitle("Decaf");
-        		alert.setHeaderText("File error");
-        		alert.setContentText("There is nothing in the file to save");
+                Alert warning = new Alert(AlertType.ERROR);
+                warning.setTitle("Decaf");
+                warning.setHeaderText(path);
+                warning.setContentText("There is nothing in the file to save");
 
-        		alert.showAndWait();
+                warning.showAndWait();
             }
         }
 
@@ -150,48 +150,95 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         }
 
     }
-	
-	 public void exitOptions() {//Need to fix thie exit command because it doesn't close the window
-		
-	        textBeforeExit = text.getText();
-	      
-	        if (textUponOpen.equals(textBeforeExit)) {//no changes have been made, don't need to prompt extra save
-	        	
-	            //System.exit(0);
-	            windowNum--;
 
-	            //check if more than one window is open
-	        }
-	        else{
-	        		
-	            //Changes have been made, prompt user to save
-	            Alert alert = new Alert(AlertType.CONFIRMATION);
-	            alert.setTitle("Confirmation Dialog");
-	            alert.setHeaderText("Memory loss warning");
-	            alert.setContentText("You've changed your document. Would you like to save before exiting?");
-	            Optional<ButtonType> result = alert.showAndWait();
-	            if (result.get() == ButtonType.OK) {
-	                //reroute to save
-	                saveFile();
-	            
-	                
-	                
+    public void exitOptions() {//Need to fix thie exit command because it doesn't close the window
 
-	            } else if (result.get() == ButtonType.CANCEL) {
-	            	
-	            	
-	                //System.exit(0);
-	                windowNum--;
-	            }
-	            else
-	            {
-	            	
-	            }
-	        }
-	        if(windowNum == 0){
-	            System.exit(0);
-	        }
-	    }
+        textBeforeExit = text.getText();
+
+        if (textUponOpen.equals(textBeforeExit)) {//no changes have been made, don't need to prompt extra save
+
+            //System.exit(0);
+            Stage newStage = new Stage();
+            newStage.close();
+            windowNum--;
+
+            //check if more than one window is open
+        }
+        else{
+
+            //Changes have been made, prompt user to save
+            Alert warning = new Alert(AlertType.CONFIRMATION);
+            warning.setTitle("Confirmation Dialog");
+            warning.setHeaderText("Memory loss warning");
+            warning.setContentText("You've changed your document. Would you like to save before exiting?");
+            Optional<ButtonType> entry = warning.showAndWait();
+            if (entry.get() == ButtonType.YES) {
+                //reroute to save
+
+                saveFile();
+
+
+
+
+
+            } else if (entry.get() == ButtonType.NO) {
+
+
+                //System.exit(0);
+
+                windowNum--;
+            }
+            else
+            {
+                Stage newStage = new Stage();
+                newStage.close();
+            }
+        }
+        if(windowNum == 0){
+            System.exit(0);
+        }
+    }
+    public void RemoveFile()
+    {
+        FileChooser jfc = new FileChooser();//directs user to home directory
+        jfc.setTitle("Select a java file");//dialog for selecting file will say choose file
+        jfc.setInitialDirectory(new File("."));
+        jfc.getExtensionFilters().addAll(new ExtensionFilter("Java (.java)", "*.java"));
+        File returnValue = jfc.showOpenDialog(null);
+
+        if (returnValue != null)
+        {
+
+
+            Alert warning = new Alert(Alert.AlertType.CONFIRMATION);
+            warning.setTitle("File Warning");
+            warning.setHeaderText("The file you selected "+returnValue.getName()+" will be deleted");
+            warning.setContentText("Are you sure you want to delete " +returnValue.getName());
+            Optional<ButtonType> entry = warning.showAndWait();
+
+            if (entry.get() == ButtonType.OK)
+            {
+
+                returnValue.delete();//deletes the file
+                Alert warning2 = new Alert(AlertType.INFORMATION);
+                warning2.setTitle("Confirmation");
+                warning2.setHeaderText("The file you selected "+returnValue.getName()+" has been deleted ");
+                warning2.setContentText("Please hit the Ok button to close the dialog");
+                warning2.showAndWait();
+
+
+            }
+        }
+        /*else
+        {
+            Alert warning = new Alert(AlertType.INFORMATION);
+            warning.setTitle("Decaf");
+            warning.setHeaderText("File cancelation");
+            warning.setContentText("No file was selected to delete");
+            warning.showAndWait();
+        }*/
+
+    }
     void saveAsFile(){
         //ToDo: Implement save so you can save changes to the same file repeatedly
         //Save currently only works as a "Save as"
@@ -206,7 +253,7 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         File returnValue = jfc.showSaveDialog(null);
         if (returnValue !=null)
         {//input here a file read so that it can open and read the file
-          
+
             try//checks if the file is able to open and not invalid
             {
 
@@ -231,21 +278,21 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
             }
             catch(Exception evt)
             {
-            	Alert alert = new Alert(AlertType.ERROR);
-        		alert.setTitle("Decaf");
-        		alert.setHeaderText("File error");
-        		alert.setContentText("There is nothing in the file to save");
+                Alert warning = new Alert(AlertType.ERROR);
+                warning.setTitle("Decaf");
+                warning.setHeaderText("File error");
+                warning.setContentText("There is nothing in the file to save");
 
-        		alert.showAndWait();
+                warning.showAndWait();
             }
         }
         //else {
         //    JOptionPane.showMessageDialog(title, "You canceled to save a file"); //message appears when canceling
         //}
     }
-	@Override
-	 public void handle(ActionEvent event) 
-    { 
+    @Override
+    public void handle(ActionEvent event)
+    {
         String in = ((MenuItem)event.getSource()).getText();
         if(in.equals("Open"))
         {
@@ -254,44 +301,48 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
             jfc.setInitialDirectory(new File("."));
             jfc.getExtensionFilters().addAll(new ExtensionFilter("Java (.java)", "*.java"));
             File returnValue = jfc.showOpenDialog(null);
-            if (returnValue != null) 
+            if (returnValue != null)
             {
-            	
-                
-              
-            	//File select = new File(jfc.getInitialDirectory().getAbsolutePath()); //finds selected file so that it be open
-                	try
-                	{
-                		File file = jfc.getInitialDirectory();//gets the file name
-                        path = file.getPath();//gets the path of the file
-                        String Line = "";
-                        FileReader read = new FileReader(returnValue);//reads the file that is being open
-                        BufferedReader input = new BufferedReader(read);//shows the input of what the user used
-                        text.setText("");
-                        while ((Line = input.readLine()) != null) //reads the line
-                        {
 
 
-                            text.appendText(Line);//appends the text
-                            text.appendText("\n");//creates a new line
-                           
 
-                        }
-
-                        input.close(); //closes the file
-                	}
-                	catch(Exception evt)
+                //File select = new File(jfc.getInitialDirectory().getAbsolutePath()); //finds selected file so that it be open
+                try
+                {
+                    //File file = returnValue.getAbsolutePath();//gets the file name
+                    path = returnValue.getAbsolutePath();//gets the path of the file
+                    String Line = "";
+                    FileReader read = new FileReader(returnValue);//reads the file that is being open
+                    BufferedReader input = new BufferedReader(read);//shows the input of what the user used
+                    text.setText("");
+                    while ((Line = input.readLine()) != null) //reads the line
                     {
-                		Alert alert = new Alert(AlertType.ERROR);
-                		alert.setTitle("Decaf");
-                		alert.setHeaderText("File error");
-                		alert.setContentText("No such file exists to open");
 
-                		alert.showAndWait();
-                		
+
+                        text.appendText(Line);//appends the text
+                        text.appendText("\n");//creates a new line
+
+
                     }
+
+                    input.close(); //closes the file
                 }
-            	textUponOpen = text.getText();
+                catch(Exception evt)
+                {
+                    Alert warning = new Alert(AlertType.ERROR);
+                    warning.setTitle("Decaf");
+                    warning.setHeaderText("File error");
+                    warning.setContentText("No such file exists to open");
+
+                    warning.showAndWait();
+
+                }
+            }
+            textUponOpen = text.getText();
+        }
+        else if(in.equals("Remove File"))//Removes a file
+        {
+            RemoveFile();
         }
         else if (in.equals("Save As"))//saves only java files
         {
@@ -300,30 +351,36 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         else if (in.equals("Save")){
             saveFile();
         }
-        else if (in.equals("Exit")) 
+        else if (in.equals("Exit"))
         {
             exitOptions();
         }
-        else if (in.equals("Cut")) 
-        { 
+        else if (in.equals("Cut"))
+        {
             text.cut(); //cuts the text
         }
-        else if (in.equals("New")) {//need to create the new function 
-           
+        else if (in.equals("New"))
+        {
+
         }
-        else if (in.equals("Copy")) 
-        { 
+        else if (in.equals("Copy"))
+        {
             text.copy(); //copies text
-        } 
-        else if (in.equals("Paste")) 
-        { 
+        }
+        else if (in.equals("Paste"))
+        {
             text.paste(); //paste the text
         }
-        
-    
+
+
     }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
+/*TODO:
+    - fix save if file is created from open function
+    -
+ */
