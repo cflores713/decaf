b1f7507e8cd0a7e733fc54607a676d97f1c1cb7e
chrismoran95 <47925970+chrismoran95@users.noreply.github.com>
2019-10-24 01:52:18 -0500

    word count added on bottom of editor/exit function fixed
    word count added on bottom of editor/exit function fixed

---
+++ MyClassLoader.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MyClassLoader extends ClassLoader{
+
+    public MyClassLoader(ClassLoader parent) {
+        super(parent);
+    }
+
+    public Class loadClassByPath(String path) throws ClassNotFoundException {
+
+        try {
+            Path myPath = Paths.get(path);
+            byte[] classData = Files.readAllBytes(myPath);
+            return defineClass(null, classData, 0, classData.length);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
