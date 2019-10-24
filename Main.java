82ae93385a0100e99464363bf76658b83c46648b
chrismoran95 <47925970+chrismoran95@users.noreply.github.com>
2019-10-24 01:48:01 -0500

    Added the word count to bottom of editor/fixed exit function
    Added the word count to bottom of editor/fixed exit function

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
-package application;
-	
+package  application;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button; 
-import javafx.scene.layout.*; 
+import javafx.scene.layout.*;
 import javafx.event.ActionEvent; 
 import javafx.stage.FileChooser;
 import javafx.stage.DirectoryChooser;
@@ -18,10 +27,19 @@ import java.io.BufferedReader;
 import java.io.*;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javax.tools.*;
 import java.io.FileReader;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Optional; 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 public class Main extends Application implements  EventHandler<ActionEvent>{
 	String textUponOpen;
     String textBeforeExit;
@@ -37,10 +55,12 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
 		  
 		    //creates the frame gui object
 		    s.setTitle("Decaf Code Editor");
+		  //ToDo: Figure out where to have this result show up inside the application, not just in console
 		
 	  
-		  
+		   
 		    text = new TextArea();
+		    Label label = new Label();
 		    windowNum = 1;
 		    textUponOpen = "";
 		    textBeforeExit = "";
@@ -64,7 +84,7 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
 		    file.getItems().add(Remove);
 		    file.getItems().add(exit);
 		    
-		    newB.setOnAction(this); //action listoners
+		    newB.setOnAction(this); //action listeners
 		    NewP.setOnAction(this);
 	        open.setOnAction(this); 
 	        save.setOnAction(this);
@@ -82,7 +102,7 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
 		    Edit.getItems().add(Copy);
 		    Edit.getItems().add(Paste);
 		    
-		    Cut.setOnAction(this); //action listoners
+		    Cut.setOnAction(this); //action listeners
 	        Copy.setOnAction(this); 
 	        Paste.setOnAction(this);
 		
@@ -95,60 +115,113 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
 		    //-----------------------------------------------------------//
 		  //----------------- Execute tab-------------------//
 		    Menu Execute = new Menu("Execute");
-		   // Execute.getItems().add(Execute);
+		    MenuItem Run = new MenuItem("Run");
+		    Execute.getItems().add(Run);
+		    Execute.setOnAction(this);
+
 		    //-----------------------------------------------------------//
-		    
+			//-----------Statistics Tab-----------------//
+			Menu Stats = new Menu("Statistics");
+			MenuItem showStats = new MenuItem("Show Code Statistics");
+			Stats.getItems().add(showStats);
+			Stats.setOnAction(this);
+
 		    MenuBar mb = new MenuBar(); 
 	
 		    // add menu to menubar 
 		    mb.getMenus().add(file); 
 		    mb.getMenus().add(Edit);
 		    mb.getMenus().add(Compile); 
-		    mb.getMenus().add(Execute); 
+		    mb.getMenus().add(Execute);
+		    mb.getMenus().add(Stats);
+		    BorderPane bPane = new BorderPane();   
 		    // create a VBox 
+		    StringBinding bind = Bindings.createStringBinding(() -> {//this function creates between the user typing and the number of characters being typed
+              
+                int charNum = 0;
+    			int lineNum = 0;
+    			int keyNum = 0;
+    			String currentCode = text.getText();
+    			String lineSplit[] = currentCode.split("\\n");//delimit by new line character
+    			for(int i = 0; i < lineSplit.length; i++)
+    			{
+    				if(!lineSplit[i].isEmpty()){//keeps us from counting blank lines towards lineNum
+    					lineNum++;
+    				}
+    				for(int j = 0; j < lineSplit[i].length(); j++)
+    				{
+    					charNum++;//read through each line and count characters
+    				}
+    			}
+    			String firstSplit[] = currentCode.split("[;]");
+    			for(int i = 0; i < firstSplit.length; i++)
+    			{
+    				//code string splits each LINE from firstSplit
+    				String codeStr[] = firstSplit[i].trim().split("[\\s\\n({})]");
+    				for(int j = 0; j < codeStr.length; j++)
+    				{
+    					if(codeStr[j].equals("if") || codeStr[j].equals("else") || codeStr[j].equals("for") || codeStr[j].equals("while"))
+    					{
+    						keyNum++;
+    					}
+    				}
+    			}
+                return "Number of characters: " + charNum + "\n"
+				+ "Number of lines: " + lineNum + "\n"
+				+ "Keywords in use (if, else, while, for): " + keyNum + "\n";
+            }, text.textProperty());
+		    label.textProperty().bind(bind);//updates the word count everytime user types
+		   
 		    VBox vb = new VBox(mb); 
-		  
 		    text.setPrefHeight(1000); 
-		    text.setPrefWidth(1000);  
+		    text.setPrefWidth(1000); 
 		    vb.getChildren().add(text);
+		    vb.getChildren().add(bPane);//adds the panel to the frame of the gui
+		    bPane.setBottom(label); //sets the word count panel at the bottom
 		    Scene sc = new Scene(vb, 500,500); 
 		    // set the scene 
 		    s.setScene(sc); 
 		    
 		    s.show(); 
-		    
-		    
 		
+		    s.setOnCloseRequest( ev -> {//when user clicks the exit window button a prompt will show if they would like to save what they have
+		     exitOptions();//Need to fix the cancel button
+		    });
 		
 	}
 	
-	        
-	    
+	 
+
+	void throwMyException(Exception evt)
+    {
+        Alert warning = new Alert(AlertType.ERROR);
+        warning.setTitle("Decaf");
+        warning.setHeaderText("File error");
+        warning.setContentText("There is nothing in the file to save");
+
+        warning.showAndWait();
+    }
+
 	void saveFile()
 	{
         if (!path.equals(""))
         {
-    	  
             try//checks if the file is able to open and not invalid
             {
                 File file = new File(path);
-                path = file.getPath();//gets the path of the file
+//                path = file.getPath();//gets the path of the file
+				System.out.println(file.getName());
                 BufferedWriter writeb = new BufferedWriter(new FileWriter(file)); //creates the file with a .java extension
 
                 writeb.write(text.getText()); //gets the text that is inputed
 
                 writeb.flush();
                 writeb.close(); //closes the file
-                textUponOpen = text.getText();
+                textUponOpen = text.getText(); // Reset textUponOpen after saving changes
             }
             catch(Exception evt)
             {
-            	Alert warning = new Alert(AlertType.ERROR);
-        		warning.setTitle("Decaf");
-        		warning.setHeaderText("File error");
-        		warning.setContentText("There is nothing in the file to save");
-
-        		warning.showAndWait();
+                throwMyException(evt);
             }
         }
 
@@ -158,8 +231,20 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         }
 
     }
-	
-	 public void exitOptions() {//Need to fix thie exit command because it doesn't close the window
+	 public TreeItem<String> getNodesForDirectory(File directory) { //Returns a TreeItem representation of the specified directory
+	        TreeItem<String> root = new TreeItem<String>(directory.getName());
+	        for(File c: directory.listFiles()) {
+	            System.out.println("Loading " + c.getName());
+	            if(c.isDirectory()) { //Then we call the function recursively
+	                root.getChildren().add(getNodesForDirectory(c));
+	            } else {
+	                root.getChildren().add(new TreeItem<String>(c.getName()));
+	            }
+	        }
+	        return root;
+	 		
+	 }
+	 public void exitOptions() {
 		
 	        textBeforeExit = text.getText();
 	      
@@ -180,16 +265,12 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
 	            warning.setHeaderText("Memory loss warning");
 	            warning.setContentText("You've changed your document. Would you like to save before exiting?");
 	            Optional<ButtonType> entry = warning.showAndWait();
-	            if (entry.get() == ButtonType.YES) {
+	            if (entry.get() == ButtonType.OK) {
 	                //reroute to save
+	            	saveFile();
 	            	
-	                saveFile();
-	              
-	            
-	                
-	                
-
-	            } else if (entry.get() == ButtonType.NO) {
+	            } 
+	            else if (entry.get() == ButtonType.CANCEL) {
 	            	
 	            	
 	                //System.exit(0);
@@ -204,7 +285,9 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
 	        }
 	        if(windowNum == 0){
 	            System.exit(0);
+	  
 	        }
+	    
 	 }
 	 public void RemoveFile()
 	 {
@@ -216,8 +299,6 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
        
          if (returnValue != null) 
          {
-         	
-             
         	  Alert warning = new Alert(Alert.AlertType.CONFIRMATION);
               warning.setTitle("File Warning");
               warning.setHeaderText("The file you selected "+returnValue.getName()+" will be deleted");
@@ -233,8 +314,6 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
                       warning2.setHeaderText("The file you selected "+returnValue.getName()+" has been deleted ");
                       warning2.setContentText("Please hit the Ok button to close the dialog");
                       warning2.showAndWait();
-              
-              		
               }
           } 
          else 
@@ -248,61 +327,92 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
       
 	}
 	 void saveAsFile(){
-	        //ToDo: Implement save so you can save changes to the same file repeatedly
-	        //Save currently only works as a "Save as"
-	        //If you type into a file and save it, then exit and come back, it definitely saves
-	        //But if you want to make changes to that already-saved file, you can't. You'll have to save it as a diff version.
-
-
 	        FileChooser jfc = new FileChooser();//directs user to home directory
 	        jfc.setTitle("Save as java file");//dialog for selecting file will say choose file
 	        jfc.setInitialDirectory(new File("."));
 	        jfc.getExtensionFilters().addAll(new ExtensionFilter("Java (.java)", "*.java"));
-	        File returnValue = jfc.showSaveDialog(null);
+	        File returnValue = jfc.showSaveDialog(null);	// returnValue is either what the user selects or types
+
 	        if (returnValue !=null)
 	        {//input here a file read so that it can open and read the file
-	          
 	            try//checks if the file is able to open and not invalid
 	            {
+	            	// Need to update path variable
 
-
-	                File file = jfc.getInitialDirectory();//gets the file name
-	                path = file.getPath();//gets the path of the file
-	                if(!path.toLowerCase().endsWith(".java"))//if the file isn't a java script then create the file
+					// if file does not exist, create a new file and return the path
+					// if path does not end in .java, add .java to name and path
+	                if(!returnValue.exists())//if the file isn't a java script then create the file
 	                {
-	                    file = new File(path + ".java");
-	                    BufferedWriter writeb = new BufferedWriter(new FileWriter(returnValue)); //creates the file with a .java extension
-
-
-	                    writeb.write(text.getText()); //gets the text that is inputed
-
+						path = returnValue.getPath();//gets the path of the file
+						if(!path.toLowerCase().endsWith(".java"))
+							path += ".java";
+	                    File file = new File(path);
+	                    System.out.println("Created new file");
+	                    BufferedWriter writeb = new BufferedWriter(new FileWriter(file)); //creates the file with a .java extension
+	                    writeb.write(text.getText()); //gets the text that is input
 	                    writeb.flush();
 	                    writeb.close(); //closes the file
 	                }
+	                else {
+	                	saveFile();
+					}
 	                textUponOpen = text.getText();
-
-
-
 	            }
 	            catch(Exception evt)
 	            {
-	            	Alert alert = new Alert(AlertType.ERROR);
-	        		alert.setTitle("Decaf");
-	        		alert.setHeaderText("File error");
-	        		alert.setContentText("There is nothing in the file to save");
-
-	        		alert.showAndWait();
+                    throwMyException(evt);
 	            }
 	        }
-	        //else {
-	        //    JOptionPane.showMessageDialog(title, "You canceled to save a file"); //message appears when canceling
-	        //}
-	    }
+	    
+	 }
+	 void CheckCount()//Converted the word counter to a function so that it can be reuseable
+	 {
+			//ToDo: Figure out where to have this result show up inside the application, not just in console
+			int charNum = 0;
+			int lineNum = 0;
+			int keyNum = 0;
+			String currentCode = text.getText();
+			String lineSplit[] = currentCode.split("\\n");//delimit by new line character
+			for(int i = 0; i < lineSplit.length; i++)
+			{
+				if(!lineSplit[i].isEmpty()){//keeps us from counting blank lines towards lineNum
+					lineNum++;
+				}
+				for(int j = 0; j < lineSplit[i].length(); j++)
+				{
+					charNum++;//read through each line and count characters
+				}
+			}
+			String firstSplit[] = currentCode.split("[;]");
+			for(int i = 0; i < firstSplit.length; i++)
+			{
+				//code string splits each LINE from firstSplit
+				String codeStr[] = firstSplit[i].trim().split("[\\s\\n({})]");
+				for(int j = 0; j < codeStr.length; j++)
+				{
+					if(codeStr[j].equals("if") || codeStr[j].equals("else") || codeStr[j].equals("for") || codeStr[j].equals("while"))
+					{
+						keyNum++;
+					}
+				}
+			}
+			Alert stats = new Alert(AlertType.INFORMATION);
+			stats.setHeaderText("Code Statistics");
+			stats.setContentText("Number of characters: " + charNum + "\n"
+					+ "Number of lines: " + lineNum + "\n"
+					+ "Keywords in use (if, else, while, for): " + keyNum + "\n"
+			);
+			stats.showAndWait();
+		
+	 }
+	
 	@Override
 	 public void handle(ActionEvent event) 
     { 
         String in = ((MenuItem)event.getSource()).getText();
-        if(in.equals("Open"))
+        // Below are variables needed for build and run
+		JavaCompiler comp = ToolProvider.getSystemJavaCompiler();
+		if(in.equals("Open"))
         {
             FileChooser jfc = new FileChooser();//directs user to home directory
             jfc.setTitle("Select a java file");//dialog for selecting file will say choose file
@@ -311,13 +421,9 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
             File returnValue = jfc.showOpenDialog(null);
             if (returnValue != null) 
             {
-            	
-                
-              
-            	    //File select = new File(jfc.getInitialDirectory().getAbsolutePath()); //finds selected file so that it be open
+            	   
                 	try
                 	{
-                		
                         path = returnValue.getAbsolutePath();//gets the path of the file
                         String Line = "";
                         FileReader read = new FileReader(returnValue);//reads the file that is being open
@@ -326,15 +432,10 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
                         
                         while ((Line = input.readLine()) != null) //reads the line
                         {
-
-
                             text.appendText(Line);//appends the text
                             text.appendText("\n");//creates a new line
-                           
-
                         }
                        
-
                         input.close(); //closes the file
                 	}
                 	catch(Exception evt)
@@ -345,7 +446,6 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
                 		warning.setContentText("No such file exists to open");
 
                 		warning.showAndWait();
-                		
                     }
                 }
             	textUponOpen = text.getText();
@@ -353,18 +453,19 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         if(in.equals("New Project"))//needs to be worked on
         {
         	 
-        	
+        	//What do we want to go here as opposed to just opening new?
                 
 	    }
         else if(in.equals("Remove File"))//Removes a file
         {
-          RemoveFile();
+        	RemoveFile();
         }
         else if (in.equals("Save As"))//saves only java files
         {
             saveAsFile();
         }
         else if (in.equals("Save")){
+
             saveFile();
         }
         else if (in.equals("Exit")) 
@@ -389,8 +490,46 @@ public class Main extends Application implements  EventHandler<ActionEvent>{
         { 
             text.paste(); //paste the text
         }
-        
-    
+        else if(in.equals("Compile"))
+        {
+        	// TODO: get this working so we can compile multiple files, need to figure out cleanup of testoutput directory
+//        	// Creates a directory called testoutput that contains the .class files for a compiled .java file
+//			String[] options = new String[] { "-d", "testoutput" };
+//			File[] javaFiles = new File[] { new File(path) };
+//			JavaCompiler.CompilationTask compilationTask = comp.getTask(null, null, null,
+//					Arrays.asList(options),
+//					null,
+//					fileManager.getJavaFileObjects(javaFiles)
+//			);
+//			compilationTask.call();
+			comp.run(System.in, System.out, System.err, path);
+            System.out.println("Compilation complete");
+        }
+        else if(in.equals("Execute"))
+        {
+        	// TODO: Compile if not already compile
+            try
+            {
+            	// TODO: replace hardcode for testoutput directory with something programmatic, grab the names for loadClass after build
+				if (path.length() > 0) {
+					ClassLoader parentClassLoader = MyClassLoader.class.getClassLoader();
+					MyClassLoader classLoader = new MyClassLoader(parentClassLoader);
+					Class compiledClass = classLoader.loadClassByPath(path.replace(".java", ".class"));
+					Method main = compiledClass.getDeclaredMethod("main", String[].class);
+					String[] params = null; // init params accordingly
+					main.invoke(null, (Object) params); // static method doesn't have an instance
+				}
+            }
+            catch(Exception e)
+            {
+            	System.out.println("Run failed.");
+            	e.printStackTrace();
+            }
+        }
+        else if(in.equals("Statistics"))
+		{
+		CheckCount();//calls the word count function
+		}
     }
 
 	public static void main(String[] args) {
