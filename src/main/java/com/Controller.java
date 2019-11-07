dd8911f67c3d8912aae5219af11d7b750606eb96
pbyrnes01 <44732273+pbyrnes01@users.noreply.github.com>
2019-11-07 16:02:21 -0600

    fixed open function to open in new tab, added stats, added a bunch of comments

--- src/main/java/com/Controller.java
+++ src/main/java/com/Controller.java
@@ -39,21 +39,22 @@ public class Controller {
     public TabPane fileTabPane;
 
     public void initialize() throws IOException{
-        newFile();
+        newFile();//start with a blank file
     }
 
-
+    //Creates new blank tab in editor
     @FXML
     public void newFile() throws IOException{
-        Tab tab = new Tab("Untitled");
-        newTab(tab);
+        newTab(new Tab("Untitled"));//create new tab
     }
 
+    //create tab and sets as current or inFocus tab
     public void newTab(Tab tab) throws IOException {
         FXMLLoader loader = new FXMLLoader(new File("./src/main/resources/textEditor.fxml").toURI().toURL());
         tab.setContent(loader.load());
         EditorController controller = loader.getController();
 
+        //Code area listener, assigns text to code area based on tab path
         tab.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
             if (isNowSelected) {
                 currentTab = controller.getTabModel();
@@ -61,10 +62,12 @@ public class Controller {
             }
         });
 
-        fileTabPane.getTabs().add(tab);
-        fileTabPane.getSelectionModel().select(tab);
+        fileTabPane.getTabs().add(tab);//add new tab to tabPane
+        fileTabPane.getSelectionModel().select(tab);//set new tab to focus
     }
 
+    //Uses file explorer to open a file or project
+    //TODO: implement open to also open a project, currently just a file
     @FXML
     public void open(ActionEvent event) throws IOException {
         FileChooser jfc = new FileChooser();//directs user to home directory
@@ -73,6 +76,7 @@ public class Controller {
         jfc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Java (.java)", "*.java"));
         File returnValue = jfc.showOpenDialog(null);
 
+        //check if file exists, then either create tab or replace tab
         if (returnValue != null) {
             String tabName = fileTabPane.getSelectionModel().getSelectedItem().getText();
             System.out.println(tabName);
@@ -80,21 +84,23 @@ public class Controller {
             String parentFolder = returnValue.getParent();
 
             try {
-                //tab is empty, replace with open file and its contents
-                if (tabName.contains("Untitled") && currentTab.text.getLength() == 0){
-                    String line = "";
-                    FileReader read = new FileReader(returnValue);//reads the file that is being open
-                    BufferedReader input = new BufferedReader(read);//shows the input of what the user used
-                    currentTab.text.clear();
-                    while ((line = input.readLine()) != null) //reads the line
-                    {
-                        currentTab.text.appendText(line);//appends the text
-                        currentTab.text.appendText("\n");//creates a new line
-                    }
-                    input.close(); //closes the file
-                    //currentText.set(String.join("\n", Files.readAllLines(path)));
-                    currentTab.path = returnValue.toPath();
+                //if tab is not empty, create new tab and make current tab
+                if (currentTab.text.getLength() > 0){
+                    newTab(new Tab(path.toString())); //
                 }
+
+                //fill code area
+                String line = "";
+                FileReader read = new FileReader(returnValue);//reads the file that is being open
+                BufferedReader input = new BufferedReader(read);//shows the input of what the user used
+                currentTab.text.clear();
+                while ((line = input.readLine()) != null) //reads the line
+                {
+                    currentTab.text.appendText(line);//appends the text
+                    currentTab.text.appendText("\n");//creates a new line
+                }
+                input.close(); //closes the file
+                currentTab.path = returnValue.toPath(); //set current tab path
 
             }
             catch(Exception evt) {
@@ -103,9 +109,10 @@ public class Controller {
                 warning.setContentText("Unable to load file " + path);
                 warning.showAndWait();
             }
-            fileTabPane.getSelectionModel().getSelectedItem().setText(path.getFileName().toString());
+            String pathName = path.getFileName().toString();
+            fileTabPane.getSelectionModel().getSelectedItem().setText(pathName);//set tab title to file name
 
-
+            //TODO: create tree based on path of file
 //            createTree();
 //            treeView.setRoot(rootTreeItem);
 
@@ -113,60 +120,8 @@ public class Controller {
         //textUponOpen = currentText;
     }
 
-    @FXML
-    public void build(ActionEvent event) {
-        JavaCompiler comp = ToolProvider.getSystemJavaCompiler();
-        comp.run(System.in, System.out, System.err, currentTab.path.toString());
-        System.out.println("Compilation complete");
-    }
 
-    @FXML
-    public void run(ActionEvent event) {
-        try {
-            // TODO: replace hardcode for testoutput directory with something programmatic, grab the names for loadClass after build
-            String path = currentTab.path.toString();
-            if (path.length() > 0) {
-                ClassLoader parentClassLoader = MyClassLoader.class.getClassLoader();
-                MyClassLoader classLoader = new MyClassLoader(parentClassLoader);
-                Class compiledClass = classLoader.loadClassByPath(path.replace(".java", ".class"));
-                Method main = compiledClass.getDeclaredMethod("main", String[].class);
-                String[] params = null; // init params accordingly
-                main.invoke(null, (Object) params); // static method doesn't have an instance
-            }
-        } catch (Exception e) {
-            System.out.println("Run failed.");
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    public void close(ActionEvent event) {
-        Tab tab = fileTabPane.getSelectionModel().getSelectedItem();
-        fileTabPane.getTabs().remove(tab);
-    }
-
-    @FXML
-    public void createProj(ActionEvent event) {
-
-    }
-
-    @FXML
-    public void cut(ActionEvent event) {
-        currentTab.text.cut();
-    }
-
-    @FXML
-    public void copy(ActionEvent event) {
-        currentTab.text.copy();
-    }
-
-    @FXML
-    public void paste(ActionEvent event) {
-        currentTab.text.paste();
-    }
-
-
-
+    //create tree structure
     public void createTree() throws IOException {
 
         // create root
@@ -178,6 +133,7 @@ public class Controller {
 
     }
 
+    //iterate through directory structure and create file tree
     public void createTree(TreeItem<Path> treeItem) throws IOException{
         try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(treeItem.getValue())) {
 
@@ -200,16 +156,124 @@ public class Controller {
 
     }
 
+    //Compile system
+    @FXML
+    public void build(ActionEvent event) {
+        JavaCompiler comp = ToolProvider.getSystemJavaCompiler();
+        comp.run(System.in, System.out, System.err, currentTab.path.toString());
+        System.out.println("Compilation complete");
+    }
+
+    //Compute stats of file
+    /* TODO: add new feature 1 here
+     */
+    @FXML
+    public void computeStats(ActionEvent event) {
+        int charNum = 0;
+        int lineNum = 0;
+        int keyNum = 0;
+        String currentCode = currentTab.text.getText();
+        String lineSplit[] = currentCode.split("\\n");//delimit by new line character
+        for(int i = 0; i < lineSplit.length; i++)
+        {
+            if(!lineSplit[i].isEmpty()){//keeps us from counting blank lines towards lineNum
+                lineNum++;
+            }
+            for(int j = 0; j < lineSplit[i].length(); j++)
+            {
+                charNum++;//read through each line and count characters
+            }
+        }
+        String firstSplit[] = currentCode.split("[;]");
+        for(int i = 0; i < firstSplit.length; i++)
+        {
+            //code string splits each LINE from firstSplit
+            String codeStr[] = firstSplit[i].trim().split("[\\s\\n({})]");
+            for(int j = 0; j < codeStr.length; j++)
+            {
+                if(codeStr[j].equals("if") || codeStr[j].equals("else") || codeStr[j].equals("for") || codeStr[j].equals("while"))
+                {
+                    keyNum++;
+                }
+            }
+        }
+        Alert stats = new Alert(Alert.AlertType.INFORMATION);
+        stats.setHeaderText("Code Statistics");
+        stats.setContentText("Number of characters: " + charNum + "\n"
+                + "Number of lines: " + lineNum + "\n"
+                + "Keywords in use (if, else, while, for): " + keyNum + "\n"
+        );
+        stats.showAndWait();
+    }
+
+
+    //Execute main program
+    /* TODO: replace hardcode for testoutput directory with something programmatic, grab the names for loadClass after build
+                    - currently only executes file in focus, needs to run based on project
+     */
+    @FXML
+    public void run(ActionEvent event) {
+        try {
+            String path = currentTab.path.toString();
+            if (path.length() > 0) {
+                ClassLoader parentClassLoader = MyClassLoader.class.getClassLoader();
+                MyClassLoader classLoader = new MyClassLoader(parentClassLoader);
+                Class compiledClass = classLoader.loadClassByPath(path.replace(".java", ".class"));
+                Method main = compiledClass.getDeclaredMethod("main", String[].class);
+                String[] params = null; // init params accordingly
+                main.invoke(null, (Object) params); // static method doesn't have an instance
+            }
+        } catch (Exception e) {
+            System.out.println("Run failed.");
+            e.printStackTrace();
+        }
+    }
+
+    //close tab/file in focus
+    /*TODO: check if text changed and signal if user wants to save
+          *idea: have variable in editorController and check that
+     */
+    @FXML
+    public void close(ActionEvent event) {
+        Tab tab = fileTabPane.getSelectionModel().getSelectedItem();
+        fileTabPane.getTabs().remove(tab);
+    }
+
+
+    @FXML
+    public void createProj(ActionEvent event) {
+
+    }
+
+    //basic utility editing functions for text
+    @FXML
+    public void cut(ActionEvent event) {
+        currentTab.text.cut();
+    }
+
+    @FXML
+    public void copy(ActionEvent event) {
+        currentTab.text.copy();
+    }
+
+    @FXML
+    public void paste(ActionEvent event) {
+        currentTab.text.paste();
+    }
+
+
+
+    //Replace contents of file in directory with contents in editor
     @FXML
     public void save(ActionEvent event) {
-        if (!currentTab.path.equals(""))
+        if (!currentTab.path.equals(""))//checks if current tab has a path
         {
             try//checks if the file is able to open and not invalid
             {
                 File file = new File(currentTab.path.toString());
                 BufferedWriter writeb = new BufferedWriter(new FileWriter(file)); //creates the file with a .java extension
 
-                writeb.write(currentTab.text.getText()); //gets the text that is inputed
+                writeb.write(currentTab.text.getText()); //gets the text that is inputted
 
                 writeb.flush();
                 writeb.close(); //closes the file
@@ -223,13 +287,14 @@ public class Controller {
                 warning.showAndWait();
             }
         }
-
+        //there is no path, call saveAs function
         else
         {
             saveAs();
         }
     }
 
+    //Create file in directory structure to save contents in editor
     @FXML
     public void saveAs() {
         FileChooser jfc = new FileChooser();//directs user to home directory
